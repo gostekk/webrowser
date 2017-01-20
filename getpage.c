52d277f3479df2c16b6581c7bc641a9d246db7c1
gostekk <gostekk94@gmail.com>
2017-01-20 20:37:19 +0100

    Modified getpage.c | Added argv variables

--- getpage.c
+++ getpage.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 #include <curl/curl.h>
+
+
 
 int main(int argc, char **argv)
 {
     CURL *curl;
     FILE *fp;
     CURLcode res;
-    char *url = argv[1];
-    char outfilename[FILENAME_MAX] = "page.html";
+    char *url = argv[1], outfile[FILENAME_MAX];
+    strcpy(outfile,argv[2]);
     curl = curl_easy_init();
     if (curl)
     {
-        fp = fopen(outfilename,"wb");
+        fp = fopen(outfile,"wb");
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
